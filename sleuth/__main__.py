3674b210964be58d243943e09d0e765b39b2047a
Morgan Robertson <robertson.md7@gmail.com>
2014-09-27 21:22:13 -0500

    Execute config file and script.
    Sleuth processes the config file and executes the main script.
    Currently, Sleuth decorators only work for functions defined in modules
    outside of the main script.

--- sleuth/__main__.py
+++ sleuth/__main__.py
@@ -11,30 +11,57 @@ def _parse_args():
                                      'diagnostic tool for Python 3.x')
     parser.add_argument('--version', '-v', action='version',
                         version='Sleuth {0}'.format(__version__))
+    parser.add_argument('--config', '-c', metavar='SLEUTHCONFIG',
+                        default='sleuthconfig.py',
+                        help='A Sleuth configuration file')
+    parser.add_argument('--preserve', '-p', action='store_true',
+                        help='Preserve the execution environment between the '
+                        'execution of the configuration file and the '
+                        'execution of the Python file')
     parser.add_argument('pyfile', help='The Python script to run')
     parser.add_argument('progargs', metavar='arg', nargs='*',
                         help='An argument for the Python script')
+
     args = parser.parse_args()
-
-    print(args.pyfile)
-    print(args.progargs)
-
     return args
 
 
-def _run(pyfile):
+def _run(configFile, pyfile, preserve=False):
+    def cleanup():
+        # Set the context for executing the Python file. This cleans up some of
+        # the effects to __main__.__dict__ caused by Sleuth.
+        import __main__
+        exec_context = {'__name__': '__main__', '__file__': pyfile,
+                        '__builtins__': __builtins__}
+        __main__.__dict__.clear()
+        __main__.__dict__.update(exec_context)
+        g = l = __main__.__dict__
+        return g, l
+
+    #g = globals()
+    #l = locals()
+
+    # Execute the config file
+    with open(configFile, 'rb') as f:
+        code = compile(f.read(), configFile, 'exec')
+    g, l = cleanup()
+    #g, l = (g, l) if preserve else cleanup()
+    exec(code, g, l)
+    #import sleuthconfig
+
+    # Execute the Python file
     with open(pyfile, 'rb') as f:
         code = compile(f.read(), pyfile, 'exec')
+    g, l = (g, l) if preserve else cleanup()
+    exec(code, g, l)
 
-    # Set the context for executing the Python file. This cleans up some of
-    # the effects to __main__.__dict__ caused by Sleuth.
-    import __main__
-    exec_context = {'__name__': '__main__', '__file__': pyfile,
-                    '__builtins__': __builtins__}
-    __main__.__dict__.clear()
-    __main__.__dict__.update(exec_context)
-    globals = locals = __main__.__dict__
-    exec(code, globals, locals)
+    '''
+    if preserve:
+        exec(code)
+    else:
+        globals, locals = cleanup()
+        exec(code, globals, locals)
+    '''
 
 
 def main():
@@ -45,12 +72,13 @@ def main():
 
     args = _parse_args()
     pyfile = args.pyfile
+    config = args.config
     sys.argv[:] = [pyfile] + args.progargs
 
     if not os.path.exists(pyfile):
         raise SystemExit('Error: {0} does not exist.'.format(pyfile))
 
-    _run(pyfile)
+    _run(config, pyfile, args.preserve)
 
 
 if __name__ == '__main__':
