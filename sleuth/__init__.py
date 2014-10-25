5c83dd41ced0d3f3385c0cd17cd1a626067d8b01
Morgan Robertson <robertson.md7@gmail.com>
2014-10-25 13:53:34 -0500

    Bump version to 0.2.0 development.

--- sleuth/__init__.py
+++ sleuth/__init__.py
@@ -8,7 +8,7 @@ if sys.version_info[:2] < (3, 0):
     raise ImportError("Sleuth requires Python 3.")
 del sys
 
-__version__ = '0.1.0'
+__version__ = '0.2.0d'
 
 from .__main__ import main
 from .error import *
