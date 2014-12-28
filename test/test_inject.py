154416c06ca3ab3986622062638469a16f9f76f6
Morgan Robertson <robertson.md7@gmail.com>
2014-12-28 14:47:27 -0600

    Cleanup tests.

--- test/test_inject.py
+++ test/test_inject.py
@@ -19,13 +19,16 @@ from sleuth.inject import _Break, _Call, _Inject, _Log, _Print
 class TestInjectionActions(unittest.TestCase):
     def setUp(self):
         reload(logging)
-        self.LOGNAME = 'testlog'
-        self.LOG = StringIO()
-        logging.basicConfig(level=logging.DEBUG, stream=self.LOG)
+        self.frame = self._get_test_frame()
+        self.test_str = 'INJECTION TEST'
+        self.fmt_str = '{message} {magic_number}'
+        self.log_name = 'testlog'
+        self.log = StringIO()
+        logging.basicConfig(level=logging.DEBUG, stream=self.log)
 
     def tearDown(self):
-        self.LOGNAME = None
-        self.LOG = None
+        self.log_name = None
+        self.log = None
         logging = None
 
     def _get_test_frame(self):
@@ -44,93 +47,57 @@ class TestInjectionActions(unittest.TestCase):
 
     def test_Print(self):
         with patch('sys.stdout', new=StringIO()) as fake_stdout:
-            # Create the action
-            fmtStr = 'PRINT INJECTION TEST'
-            action = _Print(fmtStr)
+            action = _Print(self.test_str)
+            action(self.frame)
 
-            # Perform the action
-            frame = self._get_test_frame()
-            expected_out = fmtStr
-            action(frame)
-
-            self.assertEqual(fake_stdout.getvalue().strip(), expected_out)
+            self.assertEqual(fake_stdout.getvalue().strip(), self.test_str)
 
     def test_Print_formatting(self):
         with patch('sys.stdout', new=StringIO()) as fake_stdout:
-            # Create the action
-            fmtStr = '{message} {magic_number}'
-            action = _Print(fmtStr)
-
-            # Perform the action
-            frame = self._get_test_frame()
-            expected_out = '{message} {magic_number}'.format(**frame.f_locals)
-            action(frame)
+            action = _Print(self.fmt_str)
+            expected_out = self.fmt_str.format(**self.frame.f_locals)
+            action(self.frame)
 
             self.assertEqual(fake_stdout.getvalue().strip(), expected_out)
 
     def test_Print_to_file(self):
         fake_open = mock_open(mock=MagicMock())
         with patch('sleuth.inject.open', fake_open, create=True):
-            # Create the action
-            fmtStr = '{message} {magic_number}'
-            action = _Print(fmtStr, file='junk.txt')
-
-            # Perform the action
-            frame = self._get_test_frame()
-            expected_out = '{message} {magic_number}'.format(**frame.f_locals)
-            action(frame)
+            action = _Print(self.fmt_str, file='junk.txt')
+            expected_out = self.fmt_str.format(**self.frame.f_locals)
+            action(self.frame)
 
             fake_file = fake_open.return_value.__enter__.return_value
             fake_file.write.assert_any_call(expected_out)
 
     def test_Call(self):
-        # Create the action
         func = MagicMock()
         action = _Call(func, 'message', kwarg='magic_number')
-
-        # Perform the action
-        frame = self._get_test_frame()
-        message = frame.f_locals['message']
-        magic_number = frame.f_locals['magic_number']
-        action(frame)
+        message = self.frame.f_locals['message']
+        magic_number = self.frame.f_locals['magic_number']
+        action(self.frame)
 
         func.assert_called_once_with(message, kwarg=magic_number)
 
     def test_Log(self):
-        # Create the action
-        fmtStr = 'LOG INJECTION TEST'
-        action = _Log(fmtStr)
+        action = _Log(self.test_str)
+        action(self.frame)
 
-        # Perform the action
-        frame = self._get_test_frame()
-        expected_out = fmtStr
-        action(frame)
-
-        self.assertRegex(self.LOG.getvalue(), expected_out)
+        self.assertRegex(self.log.getvalue(), self.test_str)
 
     def test_Log_formatting(self):
-        # Create the action
-        fmtStr = '{message} {magic_number}'
-        action = _Log(fmtStr)
+        action = _Log(self.fmt_str)
+        expected_out = self.fmt_str.format(**self.frame.f_locals)
+        action(self.frame)
 
-        # Perform the action
-        frame = self._get_test_frame()
-        expected_out = '{message} {magic_number}'.format(**frame.f_locals)
-        action(frame)
-
-        self.assertRegex(self.LOG.getvalue(), expected_out)
+        self.assertRegex(self.log.getvalue(), expected_out)
 
     def test_Log_with_logName(self):
-        # Create the action
-        fmtStr = 'LOG INJECTION TEST'
-        action = _Log(fmtStr, logName=self.LOGNAME)
+        action = _Log(self.test_str, logName=self.log_name)
+        expected_out = '\S*{0}\S*{1}'.format(self.log_name, self.test_str)
+        action(self.frame)
 
-        # Perform the action
-        frame = self._get_test_frame()
-        expected_out = '\S*{0}\S*{1}'.format(self.LOGNAME, fmtStr)
-        action(frame)
-
-        self.assertRegex(self.LOG.getvalue(), expected_out)
+        self.assertRegex(self.log.getvalue(), expected_out)
 
 
 if __name__ == '__main__':
